cd800132fc1f2bb8eb1d7d9abac54226daf44ca9
deependujha <deependujha21@gmail.com>
2022-08-06 18:39:41 +0530

    added bootstrap into the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useEffect } from "react";
+import "../components/layouts/Navbar";
+import "../components/layouts/Footer";
+import Footer from "../components/layouts/Footer";
+import Navbar from "../components/layouts/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // to load bootstrap js
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
