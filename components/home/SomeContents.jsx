8ccf63a9efb0809bd26d76af0e70a78b4aef520d
deependujha <deependujha21@gmail.com>
2022-08-07 11:54:52 +0530

    contents of home page done

---
+++ components/home/SomeContents.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SomeContents = () => {
+  return (
+    <div className="text-center my-5">
+      <h2 className="text-success">💲 Welcome to Inno Bank 💲</h2>
+
+      <h5 className="text-primary">
+        A bank of the community, by the community and for the community.{" "}
+      </h5>
+    </div>
+  );
+};
+
+export default SomeContents;
